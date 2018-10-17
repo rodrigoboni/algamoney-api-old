d5b1503aa986b2b91bcf11ce4755ac2b81c1f657
Rodrigo Maximiliano Boni <s2it_rboni@uolinc.com>
2018-10-17 17:15:50 -0300

    migracao intellij

--- src/main/java/com/algaworks/algamoney/api/repository/lancamento/impl/LancamentoRepositoryImpl.java
+++ src/main/java/com/algaworks/algamoney/api/repository/lancamento/impl/LancamentoRepositoryImpl.java
@@ -1,8 +1,14 @@
 package com.algaworks.algamoney.api.repository.lancamento.impl;
 
+import com.algaworks.algamoney.api.model.Lancamento;
+import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
+import com.algaworks.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -11,86 +17,77 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Pageable;
-import org.springframework.util.StringUtils;
-
-import com.algaworks.algamoney.api.model.Lancamento;
-import com.algaworks.algamoney.api.model.Lancamento_;
-import com.algaworks.algamoney.api.repository.filter.LancamentoFilter;
-import com.algaworks.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
-
 /**
  * Implementação dos filtros / queries específicas p/ entidade lancamento
- * 
- * @author s2it_rboni
  *
+ * @author s2it_rboni
  */
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
-	@PersistenceContext
-	private EntityManager manager;
-	
-	@Value("app.recordsPerPage")
-	private String defaultRecordsPerPage;
+    @PersistenceContext
+    private EntityManager manager;
 
-	@Override
-	public List<Lancamento> filter(LancamentoFilter filter, Pageable pageable) {
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
-		CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
+    @Value("app.recordsPerPage")
+    private String defaultRecordsPerPage;
 
-		Root<Lancamento> from = criteria.from(Lancamento.class);
+    @Override
+    public List<Lancamento> filter (LancamentoFilter filter, Pageable pageable) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
 
-		criteria.where(getPredicates(filter, builder, from));
-		
-		TypedQuery<Lancamento> query = manager.createQuery(criteria);
-		setPageable(query, pageable);
-		
-		return query.getResultList();
-	}
+        Root<Lancamento> from = criteria.from(Lancamento.class);
 
-	private void setPageable(TypedQuery<Lancamento> query, Pageable pageable) {
-		int page = pageable.getPageNumber() >= 0 ? pageable.getPageNumber() : 0;
-		int pageSize = pageable.getPageSize() > 0 ? pageable.getPageSize() : Integer.valueOf(defaultRecordsPerPage);
-		query.setFirstResult(page * pageSize);
-		query.setMaxResults(pageSize);
-	}
-	
-	public Long getFilterTotalRecords(LancamentoFilter filter) {
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
-		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
-		
-		Root<Lancamento> from = criteria.from(Lancamento.class);
-		
-		criteria.where(getPredicates(filter, builder, from));
-		
-		criteria.select(builder.count(from));
-		
-		return manager.createQuery(criteria).getSingleResult();
-	}
+        criteria.where(getPredicates(filter, builder, from));
 
-	private Predicate[] getPredicates(LancamentoFilter filter, CriteriaBuilder builder, Root<Lancamento> from) {
-		List<Predicate> predicates = new ArrayList<>();
-		
-		if (filter == null) {
-			return predicates.toArray(new Predicate[0]);
-		}
+        TypedQuery<Lancamento> query = manager.createQuery(criteria);
+        setPageable(query, pageable);
 
-		if (!StringUtils.isEmpty(filter.getDescricao())) {
-			//usando metamodel para não quebrar criteria caso nome do atributo na entidade seja alterado
-			//where descricao like '%<valor>%'
-			predicates.add(builder.like(builder.lower(from.get(Lancamento_.descricao)), "%" + filter.getDescricao().toLowerCase() + "%"));
-		}
+        return query.getResultList();
+    }
 
-		if (filter.getDataVencimentoDe() != null) {
-			predicates.add(builder.greaterThanOrEqualTo(from.get(Lancamento_.dataVencimento), filter.getDataVencimentoDe()));
-		}
+    private void setPageable (TypedQuery<Lancamento> query, Pageable pageable) {
+        int page = pageable.getPageNumber() >= 0 ? pageable.getPageNumber() : 0;
+        int pageSize = pageable.getPageSize() > 0 ? pageable.getPageSize() : Integer.valueOf(defaultRecordsPerPage);
+        query.setFirstResult(page * pageSize);
+        query.setMaxResults(pageSize);
+    }
 
-		if (filter.getDataVencimentoAte() != null) {
-			predicates.add(builder.lessThanOrEqualTo(from.get(Lancamento_.dataVencimento), filter.getDataVencimentoDe()));
-		}
+    public Long getFilterTotalRecords (LancamentoFilter filter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
 
-		return predicates.toArray(new Predicate[predicates.size()]);
-	}
+        Root<Lancamento> from = criteria.from(Lancamento.class);
+
+        criteria.where(getPredicates(filter, builder, from));
+
+        criteria.select(builder.count(from));
+
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
+    private Predicate[] getPredicates (LancamentoFilter filter, CriteriaBuilder builder, Root<Lancamento> from) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (filter == null) {
+            return predicates.toArray(new Predicate[0]);
+        }
+
+        if (!StringUtils.isEmpty(filter.getDescricao())) {
+            //usando metamodel para não quebrar criteria caso nome do atributo na entidade seja alterado
+            //where descricao like '%<valor>%'
+            predicates.add(builder
+                    .like(builder.lower(from.get("descricao")), "%" + filter.getDescricao().toLowerCase() + "%"));
+        }
+
+        if (filter.getDataVencimentoDe() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(from.get("dataVencimento"), filter.getDataVencimentoDe()));
+        }
+
+        if (filter.getDataVencimentoAte() != null) {
+            predicates.add(builder.lessThanOrEqualTo(from.get("dataVencimento"), filter.getDataVencimentoDe()));
+        }
+
+        return predicates.toArray(new Predicate[predicates.size()]);
+    }
 
 }
--- src/test/java/com/algaworks/algamoney/api/AlgamoneyApiResourceCategoriaTests.java
+++ src/test/java/com/algaworks/algamoney/api/AlgamoneyApiResourceCategoriaTests.java
@@ -8,11 +8,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.algaworks.algamoney.api.model.Categoria;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,9 +23,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import com.algaworks.algamoney.api.model.Categoria;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest // indica que o spring boot deve procurar por uma class anotada com @springbootapplication p/ iniciar app
@@ -38,192 +37,202 @@ public class AlgamoneyApiResourceCategoriaTests {
 	@Autowired
 	private MockMvc mockMvc;
 	private Long codigoRegistroTeste = 0L;
-	
+
 	/**
 	 * Verificar post com body vazio, deve retornar 404 + mensagem com id do erro
 	 * @throws Exception
 	 */
-	@Test
-	public void a_criarCatgoriaPostVazio() throws Exception {
-		mockMvc.perform(post("/categorias").accept(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.messages").isArray())
-			.andExpect(jsonPath("$.messages").isNotEmpty())
-			.andExpect(jsonPath("$.errorId").isNotEmpty())
-			.andExpect(status().isBadRequest())
-			.andDo(print());
-	}
-	
-	/**
-	 * Verificar post com body inválido, deve retornar 404 + mensagem com id erro
-	 * @throws Exception
-	 */
-	@Test
-	public void b_criarCategoriaAtributoInvalido() throws Exception {
-		mockMvc.perform(post("/categorias")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("{\"nome\" : \"teste\", \"chuchu\" : \"abc\"}")
-				.accept(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$.messages").isArray())
-		.andExpect(jsonPath("$.messages").isNotEmpty())
-		.andExpect(jsonPath("$.errorId").isNotEmpty())
-		.andExpect(status().isBadRequest())
-		.andDo(print());
-	}
-	
-	/**
-	 * Verificar post com atributo obrigatório vazio, deve retornar 404 + mensagem com id erro
-	 * @throws Exception
-	 */
-	@Test
-	public void c_criarCategoriaAtributoObrigatorioVazio() throws Exception {
-		mockMvc.perform(post("/categorias")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("{\"nome\" : \"\"}")
-				.accept(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$.messages").isArray())
-		.andExpect(jsonPath("$.messages").isNotEmpty())
-		.andExpect(jsonPath("$.errorId").isNotEmpty())
-		.andExpect(status().isBadRequest())
-		.andDo(print());
-	}
-	
-	/**
-	 * Verificar post válido - deve retornar 201 created + novo registro com id > 0
-	 * @throws Exception
-	 */
-	@Test
-	public void d_criarCategoria() throws Exception {
-		MvcResult result = mockMvc.perform(post("/categorias")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("{\"nome\" : \"registro teste\"}")
-				.accept(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$.codigo").exists())
-		.andExpect(jsonPath("$.codigo").isNumber())
-		.andExpect(jsonPath("$.codigo").isNotEmpty())
-		.andExpect(jsonPath("$.nome").exists())
-		.andExpect(jsonPath("$.nome").isString())
-		.andExpect(jsonPath("$.nome").isNotEmpty())
-		.andExpect(status().isCreated())
-		.andDo(print())
-		.andReturn();
+    @Test
+    public void a_criarCatgoriaPostVazio () throws Exception {
+        mockMvc.perform(post("/categorias").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.messages").isArray())
+                .andExpect(jsonPath("$.messages").isNotEmpty())
+                .andExpect(jsonPath("$.errorId").isNotEmpty())
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
 
-		List<Categoria> entities = getEntities(result);
-		assertThat(entities).isNotNull().isNotEmpty();
-		
-		codigoRegistroTeste = entities.get(0).getCodigo();
-	}
-	
-	/**
-	 * Verificar se retorna lista de registros
-	 * @throws Exception
-	 */
-	@Test
-	public void e_listarCategorias() throws Exception {
-		MvcResult result = mockMvc.perform(get("/categorias").accept(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$").isArray())
-			.andExpect(jsonPath("$").isNotEmpty())
-			.andExpect(status().isOk())
-			.andDo(print())
-			.andReturn();
-		
-		List<Categoria> entities = getEntities(result);
-		
-		assertThat(entities).isNotNull().isNotEmpty();
-	}
-	
-	/**
-	 * Verificar se retorna registro específico
-	 * @throws Exception
-	 */
-	@Test
-	public void f_listarCategoriaEspecifica() throws Exception {
-		MvcResult result = mockMvc.perform(get("/categorias/{codigo}", codigoRegistroTeste).accept(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$").isArray())
-		.andExpect(jsonPath("$").isNotEmpty())
-		.andExpect(status().isOk())
-		.andDo(print())
-		.andReturn();
-		
-		List<Categoria> entities = getEntities(result);
-		assertThat(entities).isNotNull().isNotEmpty().hasSize(1);
-		assertThat(entities.get(0).getCodigo()).isEqualTo(codigoRegistroTeste);
-	}
-	
-	/**
-	 * Verificar se retorna status correto qdo informado id inexistente
-	 * @throws Exception
-	 */
-	@Test
-	public void g_listarCategoriaInexistente() throws Exception {
-		mockMvc.perform(get("/categorias/{codigo}", -1).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNotFound())
-		.andDo(print());
-	}
-	
-	/**
-	 * Verificar se exclui registro
-	 * @throws Exception
-	 */
-	@Test
-	public void h_removeCategoria() throws Exception {
-		mockMvc.perform(delete("/categorias/{codigo}", codigoRegistroTeste).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNoContent())
-		.andDo(print());
-		
-		mockMvc.perform(get("/categorias/{codigo}", codigoRegistroTeste).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNotFound())
-		.andDo(print());
-	}
+    /**
+     * Verificar post com body inválido, deve retornar 404 + mensagem com id erro
+     *
+     * @throws Exception
+     */
+    @Test
+    public void b_criarCategoriaAtributoInvalido () throws Exception {
+        mockMvc.perform(post("/categorias")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"nome\" : \"teste\", \"chuchu\" : \"abc\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.messages").isArray())
+                .andExpect(jsonPath("$.messages").isNotEmpty())
+                .andExpect(jsonPath("$.errorId").isNotEmpty())
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
 
-	/**
-	 * Verificar status p/ exclusão registro inexistente
-	 * @throws Exception
-	 */
-	@Test
-	public void i_removeCategoriaInexistente() throws Exception {
-		mockMvc.perform(delete("/categorias/{codigo}", -1).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNotFound())
-		.andDo(print());
-	}
-	
-	/**
-	 * Método utilitário para retornar coleção de instâncias da entidade utilizada no recurso em teste
-	 * @param result
-	 * @return
-	 * @throws Exception
-	 * @throws UnsupportedEncodingException
-	 * @throws IOException
-	 */
-	private List<Categoria> getEntities(MvcResult result) throws Exception, UnsupportedEncodingException, IOException {
-		if(result == null) {
-			return null;
-		}
-		
-		ObjectMapper jsonMapper = new ObjectMapper();
-		JsonNode jsonContent = jsonMapper.readTree(result.getResponse().getContentAsString());
+    /**
+     * Verificar post com atributo obrigatório vazio, deve retornar 404 + mensagem com id erro
+     *
+     * @throws Exception
+     */
+    @Test
+    public void c_criarCategoriaAtributoObrigatorioVazio () throws Exception {
+        mockMvc.perform(post("/categorias")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"nome\" : \"\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.messages").isArray())
+                .andExpect(jsonPath("$.messages").isNotEmpty())
+                .andExpect(jsonPath("$.errorId").isNotEmpty())
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
 
-		if(jsonContent.isNull() || jsonContent.size() <= 0) {
-			return null;
-		}
-		
-		List<Categoria> entities = new ArrayList<Categoria>();
-		
-		if(jsonContent.isArray()) {
-			for (JsonNode jsonNode : jsonContent) {
-				Categoria categoria = new Categoria();
-				categoria.setCodigo(jsonNode.get("codigo").asLong());
-				categoria.setNome(jsonNode.get("nome").asText());
-				
-				entities.add(categoria);
-			}			
-		} else {
-			Categoria categoria = new Categoria();
-			categoria.setCodigo(jsonContent.get("codigo").asLong());
-			categoria.setNome(jsonContent.get("nome").asText());
-			
-			entities.add(categoria);
-		}
-			
-		return entities.isEmpty() ? null : entities;
-	}
+    /**
+     * Verificar post válido - deve retornar 201 created + novo registro com id > 0
+     *
+     * @throws Exception
+     */
+    @Test
+    public void d_criarCategoria () throws Exception {
+        MvcResult result = mockMvc.perform(post("/categorias")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"nome\" : \"registro teste\"}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo").exists())
+                .andExpect(jsonPath("$.codigo").isNumber())
+                .andExpect(jsonPath("$.codigo").isNotEmpty())
+                .andExpect(jsonPath("$.nome").exists())
+                .andExpect(jsonPath("$.nome").isString())
+                .andExpect(jsonPath("$.nome").isNotEmpty())
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn();
+
+        List<Categoria> entities = getEntities(result);
+        assertThat(entities).isNotNull().isNotEmpty();
+
+        codigoRegistroTeste = entities.get(0).getCodigo();
+    }
+
+    /**
+     * Verificar se retorna lista de registros
+     *
+     * @throws Exception
+     */
+    @Test
+    public void e_listarCategorias () throws Exception {
+        MvcResult result = mockMvc.perform(get("/categorias").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        List<Categoria> entities = getEntities(result);
+
+        assertThat(entities).isNotNull().isNotEmpty();
+    }
+
+    /**
+     * Verificar se retorna registro específico
+     *
+     * @throws Exception
+     */
+    @Test
+    public void f_listarCategoriaEspecifica () throws Exception {
+        MvcResult result = mockMvc
+                .perform(get("/categorias/{codigo}", codigoRegistroTeste).accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        List<Categoria> entities = getEntities(result);
+        assertThat(entities).isNotNull().isNotEmpty().hasSize(1);
+        assertThat(entities.get(0).getCodigo()).isEqualTo(codigoRegistroTeste);
+    }
+
+    /**
+     * Verificar se retorna status correto qdo informado id inexistente
+     *
+     * @throws Exception
+     */
+    @Test
+    public void g_listarCategoriaInexistente () throws Exception {
+        mockMvc.perform(get("/categorias/{codigo}", -1).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    /**
+     * Verificar se exclui registro
+     *
+     * @throws Exception
+     */
+    @Test
+    public void h_removeCategoria () throws Exception {
+        mockMvc.perform(delete("/categorias/{codigo}", codigoRegistroTeste).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+
+        mockMvc.perform(get("/categorias/{codigo}", codigoRegistroTeste).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    /**
+     * Verificar status p/ exclusão registro inexistente
+     *
+     * @throws Exception
+     */
+    @Test
+    public void i_removeCategoriaInexistente () throws Exception {
+        mockMvc.perform(delete("/categorias/{codigo}", -1).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    /**
+     * Método utilitário para retornar coleção de instâncias da entidade utilizada no recurso em teste
+     *
+     * @param result
+     * @return
+     * @throws Exception
+     * @throws UnsupportedEncodingException
+     * @throws IOException
+     */
+    private List<Categoria> getEntities (MvcResult result) throws Exception, UnsupportedEncodingException, IOException {
+        if (result == null) {
+            return null;
+        }
+
+        ObjectMapper jsonMapper = new ObjectMapper();
+        JsonNode jsonContent = jsonMapper.readTree(result.getResponse().getContentAsString());
+
+        if (jsonContent.isNull() || jsonContent.size() <= 0) {
+            return null;
+        }
+
+        List<Categoria> entities = new ArrayList<Categoria>();
+
+        if (jsonContent.isArray()) {
+            for (JsonNode jsonNode : jsonContent) {
+                Categoria categoria = new Categoria();
+                categoria.setCodigo(jsonNode.get("codigo").asLong());
+                categoria.setNome(jsonNode.get("nome").asText());
+
+                entities.add(categoria);
+            }
+        } else {
+            Categoria categoria = new Categoria();
+            categoria.setCodigo(jsonContent.get("codigo").asLong());
+            categoria.setNome(jsonContent.get("nome").asText());
+
+            entities.add(categoria);
+        }
+
+        return entities.isEmpty() ? null : entities;
+    }
 }
